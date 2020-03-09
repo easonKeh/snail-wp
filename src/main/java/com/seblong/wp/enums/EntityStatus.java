a919409fd7d2db2fad009811db583288b4a6dce3
王帅 <ws890810@126.com>
2020-03-09 18:52:20 +0800

    [完成開獎]

--- src/main/java/com/seblong/wp/enums/EntityStatus.java
+++ src/main/java/com/seblong/wp/enums/EntityStatus.java
@@ -2,6 +2,6 @@ package com.seblong.wp.enums;
 
 public enum EntityStatus {
 
-	ALL, ACTIVED, NOT_ACTIVED, DELETED, INVALID, SOLVE
+	ALL, ACTIVED, NOT_ACTIVED, DELETED, INVALID, SOLVE, DONE
 
 }
--- src/main/java/com/seblong/wp/services/SnailWishLotteryRecordService.java
+++ src/main/java/com/seblong/wp/services/SnailWishLotteryRecordService.java
@@ -1,12 +1,18 @@
 package com.seblong.wp.services;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 
 import com.seblong.wp.entities.SnailWishLotteryRecord;
+import com.seblong.wp.entities.WishRecord;
 import com.seblong.wp.entities.SnailWish.AwardType;
 
 public interface SnailWishLotteryRecordService {
 
+	List<SnailWishLotteryRecord> create( Collection<WishRecord> wishRecords );
+	
 	SnailWishLotteryRecord create( String userId, String lotteryDate, AwardType type );
 	
 	SnailWishLotteryRecord get( String userId, String lotteryDate );
